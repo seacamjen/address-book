dcc5ccd30359cb9af40384681a9ad6419f85ca9d
Chris Finney and Cameron Jensen <chris@>
2017-02-27 09:04:19 -0800

    add javascript functionality to display contact info in separate column and create new full-name method on Contact prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,11 @@ function Contact(first, last) {
   this.lastName = last;
 }
 
-$(function)() {
+Contact.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName;
+}
+
+$ (function(){
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -13,7 +17,14 @@ $(function)() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $(".contact").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
